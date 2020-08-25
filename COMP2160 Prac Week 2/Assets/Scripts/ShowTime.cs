a3d93c53346f695884f2df809955d2d8531ba2e7
Nat Stepan <natstepan8766@gmail.com>
2020-08-25 15:47:40 +1000

    Copy over files from zip

---
+++ COMP2160 Prac Week 2/Assets/Scripts/ShowTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTime : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("time = " + Time.time);
+        Debug.Log("deltaTime = " + Time.detlaTime);
+    }
+}
